4d17507e5aff120d1c064d2f89f5ac7144e49b62
aleksey <fanatik86@mail.ru>
2023-05-31 21:42:38 +0300

    Основа моделей для кфх

--- Home_page/models.py
+++ Home_page/models.py
@@ -1,38 +1,78 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 
 # Модель для описания сайта на главной странице
 class InfoSite(models.Model):
-    pass
+    id_info_site = models.BigAutoField(primary_key=True)
+    info_site_kfh = CKEditor5Field('Text', config_name='extends')
+
+    class Meta:
+        verbose_name = "Описание главной страницы"
+        verbose_name_plural = verbose_name
 
 
 # Модель для информации о КФХ в разделе о нас
 class AboutUs(models.Model):
-    pass
+    id_about_us = models.BigAutoField(primary_key=True)
+    about_us_kfh = CKEditor5Field('Text', config_name='extends')
+
+    class Meta:
+        verbose_name = "Описание страницы о нас"
+        verbose_name_plural = verbose_name
 
 
 # Тип для связи
 class TypePhoneKFH(models.Model):
-    pass
+    id_type_phone = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=250, name="Описание типа связи")
+
+    class Meta:
+        verbose_name = "Типы телефонов для связи"
+        verbose_name_plural = verbose_name
 
 
 # Модель Телефоны для связи
 class PhoneKFH(models.Model):
-    pass
+    id_phone = models.BigAutoField(primary_key=True)
+    number_phone = models.IntegerField(verbose_name="Телефон")
+    type_phone = models.ForeignKey(TypePhoneKFH, on_delete=models.CASCADE)
+    contacts_kfh = models.ForeignKey('ContactsKFH', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Информация о телефоне"
+        verbose_name_plural = verbose_name
 
 
 # Модель контакты
 class ContactsKFH(models.Model):
+    id_info_site = models.BigAutoField(primary_key=True)
     address_kfh = models.CharField(max_length=250)
     email_kfh = models.EmailField()
     work_kfh = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Контакты"
+        verbose_name_plural = verbose_name
+
 
 # Модель для новостей
 class NewsKFH(models.Model):
-    pass
+    id_new = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=250)
+    news = CKEditor5Field('Text', config_name='extends')
+
+    class Meta:
+        verbose_name = "Новости Акции"
+        verbose_name_plural = verbose_name
 
 
 # Карточка КФХ
 class CardKFH(models.Model):
-    pass
+    id_card = models.BigAutoField(primary_key=True)
+    inn_kfh = models.IntegerField(max_length=20, name="ИНН")
+    ogrn_kfh = models.IntegerField(max_length=30, name="ОГРН")
+
+    class Meta:
+        verbose_name = "Карточка КФХ"
+        verbose_name_plural = verbose_name
