4cc2247db7a9ace8c3a3de9026811eed89039c60
aleksey <fanatik86@mail.ru>
2023-06-04 14:22:04 +0300

    Модель продуктов проект

--- Product_KFH/models.py
+++ Product_KFH/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
-# Create your models here.
+
+class Product(models.Model):
+    category = models.OneToOneField()
+    type_product = models.OneToOneField()
+    name_product = models.CharField(max_length=200)
+    description = CKEditor5Field('Text', config_name='extends')
+    specification_product = models.ManyToManyField()
+    info_product = models.OneToOneField()
+    in_stock_product = models.IntegerField()
+    price_product = models.FloatField()
+    currency_product = models.OneToOneField()
+    pass
